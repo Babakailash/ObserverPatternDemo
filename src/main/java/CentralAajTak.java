5b648c9921be005fb16fd376bcf5fd42f180726a
Babakailash <nathkailash2020@gmail.com>
2021-06-06 01:43:55 +0530

    ObserverPattern:All observer observer and Control by Central.

---
+++ src/main/java/CentralAajTak.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CentralAajTak {
+
+     List<IObserver> observersList = new ArrayList<>();
+
+    public void register(IObserver instance) {
+        observersList.add(instance);
+    }
+
+    public void update(News news) {
+        observersList.stream()
+                .filter(object ->object.getNewsType().equals(news.getNewsType()))
+                .forEach(object -> object.onUpdate(news));
+    }
+}
---
+++ src/main/java/IObserver.java
@@ -0,0 +1,6 @@
+public  interface IObserver {
+
+     void onUpdate(News news);
+
+     NewsType getNewsType();
+}
